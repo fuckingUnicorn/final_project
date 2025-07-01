4b1a67a32bfc09d34d0c8f064ff3afe7a70f8be2
Yuriy <ramazanoff.iur@yandex.ru>
2025-07-01 22:27:19 +0300

    it works description added

--- pkg/api/updatetask.go
+++ pkg/api/updatetask.go
@@ -11,10 +11,14 @@ import (
 	"github.com/fuckingUnicorn/final_project/pkg/internal"
 )
 
+// updateTaskHandler - HTTP-обработчик для обновления существующей задачи
+// Метод: PUT
+
 func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var task *entities.Task
 	var buf bytes.Buffer
 
+	// 1. Чтение тела запроса
 	_, err := buf.ReadFrom(r.Body)
 	if err != nil {
 		err = errors.New("Error reading body: " + err.Error())
@@ -22,6 +26,7 @@ func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 2. Парсинг JSON
 	err = json.Unmarshal(buf.Bytes(), &task)
 	if err != nil {
 		err = errors.New("Error unmarshalling body: " + err.Error())
@@ -29,18 +34,21 @@ func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 3. Валидация обязательных полей
 	if task.Title == "" {
 		err = errors.New("title is required")
 		writeJSON(w, http.StatusBadRequest, err)
 		return
 	}
 
+	// 4. Проверка даты
 	if err = internal.CheckDate(task); err != nil {
 		err = errors.New("checkDate failed: " + err.Error())
 		writeJSON(w, http.StatusBadRequest, err)
 		return
 	}
 
+	// 5. Обновление в БД
 	err = db.UpdateTask(task)
 	if err != nil {
 		err = errors.New("Error adding task: " + err.Error())
@@ -48,5 +56,6 @@ func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 6. Успешный ответ
 	writeJSON(w, http.StatusOK, entities.EmptyResponse{})
 }
